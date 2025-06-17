2faaa572c2be4a1a5d05dc32d57ac7b70d5ea320
Paul Tongyoo <paul.tongyoo@gmail.com>
2025-06-16 18:43:21 -0700

    Wrapped predict function with flask; updated test.py to POST to URL

--- 04-deployment/web-service/predict.py
+++ 04-deployment/web-service/predict.py
@@ -1,6 +1,7 @@
 import pickle
+from flask import Flask, request, jsonify
 
-with open('../../../models/lin_reg.bin', 'rb') as f_in:
+with open('lin_reg.bin', 'rb') as f_in:
     (dv, model) = pickle.load(f_in)
 
 def prepare_features(ride):
@@ -13,3 +14,21 @@ def predict(features):
     X = dv.transform(features)
     preds = model.predict(X)
     return preds[0]
+
+app = Flask('duration-prediction')
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    pred = predict(features)
+    
+    result = {
+        'duration': pred,
+    }
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
