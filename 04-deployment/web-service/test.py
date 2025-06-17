9be9ace8a4b3eda52ced76b963e19c78d3c98650
Paul Tongyoo <paul.tongyoo@gmail.com>
2025-06-16 18:03:53 -0700

    Added beginnings of Module 4 (Deployment) predict web service

---
+++ 04-deployment/web-service/test.py
@@ -0,0 +1,12 @@
+import predict
+
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40
+}
+
+features = predict.prepare_features(ride)
+pred = predict.predict(ride)
+print(pred)
