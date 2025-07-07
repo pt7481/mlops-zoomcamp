0723f82320fe8031452c961da1e11a10c70d1ed1
Paul Tongyoo <paul.tongyoo@gmail.com>
2025-07-07 11:58:16 -0700

    Added missing env vars

---
+++ 06-best-practices/scripts/read_stream_prod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+STREAM_NAME="prod_ride_predictions_mlops-zoomcamp"
+SHARD_ID=$(aws kinesis describe-stream \
+  --stream-name "$STREAM_NAME" \
+  --query "StreamDescription.Shards[0].ShardId" \
+  --output text)
+
+ITER=$(aws kinesis get-shard-iterator \
+  --stream-name "$STREAM_NAME" \
+  --shard-id "$SHARD_ID" \
+  --shard-iterator-type TRIM_HORIZON \
+  --query 'ShardIterator' --output text)
+
+while [ "$ITER" != "null" ]; do
+  OUT=$(aws kinesis get-records --shard-iterator "$ITER" --limit 100)
+  
+  echo "$OUT" | jq -r '.Records[].Data' | while read -r encoded; do
+    echo "$encoded" | base64 --decode
+    echo    # new line for readability
+  done
+  
+  ITER=$(echo "$OUT" | jq -r '.NextShardIterator')
+  sleep 1
+done
+
