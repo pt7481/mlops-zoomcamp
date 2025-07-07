7e086b0b11f0521a4518977f7df4dfc05aa00467
Paul Tongyoo <paul.tongyoo@gmail.com>
2025-07-07 12:20:15 -0700

    Added script to write record to prod

--- 06-best-practices/scripts/write_stream_prod.sh
+++ 06-best-practices/scripts/write_stream_prod.sh
@@ -2,11 +2,11 @@
 
 aws kinesis put-record \
     --stream-name prod_ride_events_mlops-zoomcamp \
-    --partition-key "1" \
+    --partition-key 1 \
     --cli-binary-format raw-in-base64-out \
     --data '{"ride": {
         "PULocationID": 130,
         "DOLocationID": 205,
         "trip_distance": 3.66
     },
-    "ride_id": 156}'
+    "ride_id": 512}'
